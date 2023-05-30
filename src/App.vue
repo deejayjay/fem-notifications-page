69522d65dd7afd88b55001570146b6f093ded7f6
Deepak Joy Jose <72120524+deejayjay@users.noreply.github.com>
2023-05-29 20:57:13 -0600

    Adds the Vue boilerplate for the solution

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+  <header>
+    <h1>Notifications Page</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
